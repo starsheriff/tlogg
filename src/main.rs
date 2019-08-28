3cfa16b2f517e77749714ec40d9d394733c70a4f
Johannes Weissmann <jo@starsheriff.eu>
2019-08-28 07:59:53 +0200

    migrate on application startup

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::path::Path;
 use structopt::StructOpt;
 use xdg;
 
+/// currently used version of database schema
+pub const DB_VERSION: usize = 1;
+
+const SQL_1: &str = include_str!("../sql/01_init.sql");
+
 #[derive(Debug, StructOpt)]
 struct Tlogg {
     #[structopt(name = "verbose", short, long)]
@@ -98,6 +103,38 @@ pub fn new_connection(p: &Path) -> Result<Connection, rusqlite::Error> {
     Ok(conn)
 }
 
+
+pub fn user_version(conn: &Connection) -> rusqlite::Result<usize> {
+    let x: rusqlite::Result<i64> =
+        match conn.query_row("PRAGMA user_version", rusqlite::NO_PARAMS, |row| row.get(0)) {
+        Ok(version) => Ok(version),
+        Err(e) => Err(e),
+    };
+
+    match x {
+        Ok(version) => Ok(version as usize),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn migrate(conn: &Connection) -> rusqlite::Result<usize> {
+    let current_version: usize= user_version(conn)?;
+
+    if current_version == DB_VERSION {
+        return Ok(DB_VERSION)
+    }
+
+    if current_version < 1 {
+        // TODO: transaction
+        println!("running migration to version 1");
+        conn.execute(&format!("PRAGMA user_version = {}", 1), rusqlite::NO_PARAMS).unwrap();
+        conn.execute_batch(SQL_1)?;
+    }
+    // if new migrations are required they have to be added here
+
+    return Ok(DB_VERSION)
+}
+
 fn main() {
     let args = Tlogg::from_args();
 
@@ -118,8 +155,33 @@ fn main() {
 
     // open connection
     let conn = new_connection(db_path.as_path()).unwrap();
-    println!("{:?}", conn);
 
-    println!("{:?}", args);
-    println!("{:?}", xdg_dirs);
+    // check version and migrate if necessary
+    let user_version = user_version(&conn).unwrap();
+
+    if args.flag_verbose {
+        println!("Database schema version: {}", user_version);
+    }
+    if user_version < DB_VERSION {
+        println!("schema migration required. {}<{}", user_version, DB_VERSION);
+
+        migrate(&conn).unwrap();
+    }
+
+    // run commands
+    commands(args, conn)
+}
+
+
+fn commands(args: Tlogg, conn: rusqlite::Connection) {
+    match args.command {
+        Commands::Add(add_args) => {
+            println!("{:?}", add_args);
+            panic!("not implemented");
+        },
+        _ => {
+            panic!("not implemented");
+        },
+
+    }
 }
